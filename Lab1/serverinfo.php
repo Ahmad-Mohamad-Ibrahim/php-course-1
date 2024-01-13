95ee65c35a9a2a8fd10c49f53f658dc847b691e3
Ahmad-Mohamad-Ibrahim <ahmadmohamadibrahim11@gmail.com>
2024-01-13 12:34:34 +0200

    Solving some issues

--- Lab1/serverinfo.php
+++ Lab1/serverinfo.php
@@ -1,5 +1,10 @@
 <?php
+    // Server info 
     print ("Server Name is : " . $_SERVER["SERVER_NAME"] . "<br>");
     print ("Server Address is : " . $_SERVER["SERVER_ADDR"] . "<br>");
     print ("Server Port is : " . $_SERVER["SERVER_PORT"] . "<br>");
+
+    // File info
+    print ("Filename is : " . basename(__FILE__) .  "<br>" );
+    print ("Path of script is : " .__FILE__ . "<br>" );
 ?>
